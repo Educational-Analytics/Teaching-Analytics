c9dec35c0ffa683202551d60b67c22ba5456aed8
Dorian ROUX <mp.rouxdorian@gmail.com>
2021-05-31 13:38:38 +0200

    Test

--- Reingenieurie/Factice-Example/Programs/Quizz.py
+++ Reingenieurie/Factice-Example/Programs/Quizz.py
@@ -19,7 +19,7 @@ import os
 ################
 
 dir_path = os.path.dirname(__file__) #Find the directory path of the current python «FolderCreation» file.
-keywords = ['Reingenieurie', 'Factice-Example'] #Determine the keywords that will take the folder position.
+keywords = ['Teaching-Analytics', 'Factice-Example'] #Determine the keywords that will take the folder position.
 folder_name = 'Data' #Name of the Data folder.
 
 glob_path = '/'.join(dir_path.split('/')[(dir_path.split('/').index(keywords[0])):(dir_path.split('/').index(keywords[1]) + 1)])  #Find the General Path
@@ -51,26 +51,140 @@ random.seed( 2345 ) #The seed does not need to be randomize.
 # Factice Data #
 ################
 
-number_Quizzes = random.randint(0, 150) #Create a variable to represent the number of Quizzes.
+#Define the number of quizzes
+number_Quizzes = random.randint(100, 1000) #Create a variable to represent the number of Quizzes.
 print('Number of Quizzes:', number_Quizzes) #Display the number of Quizzes.
 
-#Defined list
+
+#Define the list of departements
+depts_type = ['Economie', 'Histoire', 'Droit', 'Management', 'Art', 'Sociologie', 'Mathématique', 'Ingénieurie', 'Philosophie', 'Informatique', 'Langues'] #Define a list of Departements
+num_depts = random.randint(ceil(number_Quizzes/150), ceil(number_Quizzes/100)) #Randomly select the number of Departements
+temp_Depts = random.sample(depts_type, num_depts) #Randomly select the Departements
+print('Number of Departement(s):', num_depts) #Display the number of Departements.
+print('List of Departements:', temp_Depts) #Display the list of Departements.
+
+
+#Define the list of years
+temp_Years = [year for year in range(2015, 2021)] #Append in a list, each year.
+print('List of Years:', temp_Years) #Display the list of Years.
+
+#Define the number of Quizzes per Departement
+avg_quiz = round(number_Quizzes/num_depts) #Find the average number of quizzes per Departement.
+print('Average Number of Quizzes per Departement:', avg_quiz) #Display the average.
+
+lst_quizz_depts = [] #Create a list representing the number of Quizzes per Departement.
+temp_quizz = number_Quizzes #Create a temporal variable.
+while sum(lst_quizz_depts) != number_Quizzes: #Create a loop that will continue until the sum of the quizzes per departement is the same as the number of quizzes
+    for len_dep in range(0, num_depts): #Create another loop to iterate the number of quizzes to each Departements.
+        numb = round(random.uniform(avg_quiz*0.90, avg_quiz*1.1)) #Select to 10% of difference from the average.
+        lst_quizz_depts.append(numb) #Append the number of quizzes into the list
+
+    if sum(lst_quizz_depts) != number_Quizzes: #Verify if the sum of the list is different from the number of quizzes previously defined.
+        lst_quizz_depts.clear() #Clear the list
+
+print('List of Number of Quizzes per Departement:', lst_quizz_depts) #Display the number of Quizzes per Departement.
+
+
+#Define a set of lists
+lst_year = []
+lst_depts = []
 lst_quiz_ID = []
+lst_chapt_ID = []
+lst_quiz_chapt = []
+lst_quiz_type = []
 lst_attempts = []
+lst_learners = []
+lst_avg_attempts = []
+lst_success = []
+lst_failure = []
+#Define arrays of choice
+chap_percent = [1] * 4 + [2] * 5 + [3] * 5 + [4] * 5 + [5] * 8 + [6] * 10 + [7] * 15 + [8] * 10 + [9] * 10 + [10] * 8 + [11] * 5 + [12] * 5 + [13] * 5 + [14] * 4
+quiztype_percent = ['QCU'] * 50 + ['QCM'] * 35 + ['QROC'] * 10 + ['QO'] * 5
+
+#Define the number of Quizzes per Chapter and their ID.
+for year in temp_Years:
+    quiz_ID = 1
+    for num_quiz, index in zip(lst_quizz_depts, range(0, len(lst_quizz_depts))):
+        num = 0
+        chap_ID = 1
+        count = 0 
+
+        while num != num_quiz:
+            ins = [quiz for quiz in range(0, len(lst_quiz_ID)) if lst_quiz_ID[quiz] == quiz_ID] if quiz_ID in lst_quiz_ID else []
+            chap_ID = lst_chapt_ID[ins[-1]] if quiz_ID in lst_quiz_ID else chap_ID
+            chapter_quiz = lst_quiz_chapt[ins[-1]] if quiz_ID in lst_quiz_ID else random.choice(chap_percent)
+            if num + chapter_quiz <= num_quiz:
+                for chap in range(0, chapter_quiz):
+                    type_quiz = lst_quiz_type[ins[-1]] if quiz_ID in lst_quiz_ID else random.choice(quiztype_percent)
+                    num_attempts = ceil(lst_attempts[ins[-1]]*1.05)  if quiz_ID in lst_quiz_ID else  random.randint(5, 100)
+
+                    if type_quiz == 'QCU':
+                        learner_done = ceil(lst_learners[ins[-1]]*1.05) if quiz_ID in lst_quiz_ID else round(random.uniform(num_attempts/3, num_attempts))
+                    else:
+                        learner_done = ceil(lst_learners[ins[-1]]*1.05) if quiz_ID in lst_quiz_ID else round(random.uniform(ceil(num_attempts/4), ceil(num_attempts/1.5)))
+                    
+                    avg_attempts = round(num_attempts / learner_done, 2)
+                    success = learner_done
+                    failure = num_attempts - learner_done
+
+
+                    #Append the Lists
+                    lst_quiz_ID.append(quiz_ID)
+                    lst_year.append(year)
+                    lst_depts.append(temp_Depts[index])
+                    lst_chapt_ID.append(chap_ID)
+                    lst_quiz_chapt.append(chapter_quiz)
+                    lst_quiz_type.append(type_quiz)
+                    lst_attempts.append(num_attempts)
+                    lst_learners.append(learner_done)
+                    lst_avg_attempts.append(avg_attempts)
+                    lst_success.append(success),
+                    lst_failure.append(failure)
+                    quiz_ID += 1
+                num += chapter_quiz
+                chap_ID += 1
+
+#Define the First DataFrame
+Quizzes_db = pd.DataFrame({'Quizz_ID': lst_quiz_ID,
+                           'Year': lst_year,
+                           'Departement': lst_depts,
+                           'Chapter_ID': lst_chapt_ID,
+                           'Quiz_perChapter': lst_quiz_chapt,
+                           'Quizz_Type': lst_quiz_type, 
+                           'Number_Attempts': lst_attempts,
+                           'UniQ_Learner_Attempts': lst_learners,
+                           'Avg_Number_Attempts': lst_avg_attempts,
+                           'Sucess_Attempts': lst_success,
+                           'Failure_Attempts': lst_failure,
+                           'Percent_Success': [round((succ/attempts)*100, 2) for succ, attempts in zip(lst_success, lst_attempts)]
+}).sort_values(by = ['Quizz_ID', 'Year'], ascending = True).set_index('Quizz_ID')
+
+print(Quizzes_db.head(5)) #Display the DataFrame.
+
+
+
+"""
+#Defined list
+
+lst_attempts = []
+lst_student = []
 lst_s_attempts = []
 lst_f_attempts = []
 lst_succes_prct = []
 lst_failure_prct = []
 lst_guess = []
-lst_sleep = []
-lst_theta = []
+lst_slip = []
+lst_b = []
+"""
 
+"""
 quiz_ID = 0
 for quiz in range(0, number_Quizzes):
     #Variables
     quiz_ID += 1 #Incremente the variable to implement the ID for each quizz
 
-    num_attempts = random.randint(1, 50)  #Define a random number of attempts for each quizz (between 1 and 50)
+    num_attempts = random.randint(1, 30)  #Define a random number of attempts for each quizz (between 1 and 50)
+    num_students = random.randint(ceil(num_attempts/4), num_attempts)
     num_success = random.randint(0, num_attempts) #Define a random number of success for each quizz (between 0 and the number of attempts)
     num_failure = num_attempts - num_success #Define a random number of failure for each quizz
 
@@ -80,36 +194,36 @@ for quiz in range(0, number_Quizzes):
     failure_ratio = round(1 - succes_ratio, 2) #Compute the ratio of failure for each quizz
     failure_percent = int(failure_ratio * 100) #Compute the percentage of failure for each quizz
 
-    #GUESS/SLEEP
+    #GUESS/slip
     #Need to be modified in a more specific way
     if succes_ratio == 0:
         ran = random.random()
         if ran >= 0.8:
-            sleep = random.uniform(0.90, 1)
+            slip = random.uniform(0.90, 1)
         elif ran >= 0.5 and ran < 0.8:
-            sleep = random.uniform(0.75, 0.90)
+            slip = random.uniform(0.75, 0.90)
         else:
-            sleep = random.uniform(0.75, 1)
+            slip = random.uniform(0.75, 1)
         guess = 0
 
     elif 0 < succes_ratio <= 0.2:
         ran = random.random()
         if ran >= 0.8:
-            sleep = random.uniform(0.75, 1)
+            slip = random.uniform(0.75, 1)
         elif ran >= 0.5 and ran < 0.8:
-            sleep = random.uniform(0.5, 0.75)
+            slip = random.uniform(0.5, 0.75)
         else:
-            sleep = random.uniform(0.5, 1)
+            slip = random.uniform(0.5, 1)
         guess = random.uniform(0, 0.2)
 
     elif succes_ratio > 0.2 and succes_ratio <= 0.5:
         ran = random.random()
         if ran >= 0.8:
-            sleep = random.uniform(0.5, 0.75)
+            slip = random.uniform(0.5, 0.75)
         elif ran >= 0.5 and ran < 0.8:
-            sleep = random.uniform(0.25, 0.50)
+            slip = random.uniform(0.25, 0.50)
         else:
-            sleep = random.uniform(0.25, 1)
+            slip = random.uniform(0.25, 1)
         guess = random.uniform(0.2, 0.5)
 
     elif succes_ratio > 0.5 and succes_ratio <= 0.8:
@@ -120,7 +234,7 @@ for quiz in range(0, number_Quizzes):
             guess = random.uniform(0.25, 0.50)
         else:
             guess = random.uniform(0.5, 1)
-        sleep = random.uniform(0.2, 0.5)
+        slip = random.uniform(0.2, 0.5)
 
     elif succes_ratio > 0.8 and succes_ratio < 1:
         ran = random.random()
@@ -130,7 +244,7 @@ for quiz in range(0, number_Quizzes):
             guess = random.uniform(0.5, 0.75)
         else:
             guess = random.uniform(0.75, 1)
-        sleep = random.uniform(0, 0.2)
+        slip = random.uniform(0, 0.2)
 
     elif succes_ratio == 1:
         ran = random.random()
@@ -140,33 +254,33 @@ for quiz in range(0, number_Quizzes):
             guess = random.uniform(0.75, 0.90)
         else:
             guess = random.uniform(0.75, 1)
-        sleep = 0
+        slip = 0
 
-    guess, sleep = round(guess, 2),  round(sleep, 2) 
+    guess, slip = round(guess, 2),  round(slip, 2) 
 
-    #Theta
-    if 0 <= guess < 0.25 and 0 < sleep <= 0.25:
-        theta = round(random.uniform(0.4, 0.6), 2)
-    elif 0.25 <= guess < 0.5 and 0 < sleep <= 0.25:
-        theta = round(random.uniform(0.4, 0.5), 2)
-    elif 0.5 <= guess < 0.75 and 0 < sleep <= 0.25:
-        theta = round(random.uniform(0.25, 0.4), 2)
-    elif 0.75 <= guess < 1 and sleep <= 0.25:
-        theta = round(random.uniform(0.1, 0.25), 2)
+    #b
+    if 0 <= guess < 0.25 and 0 < slip <= 0.25:
+        b = round(random.uniform(0.4, 0.6), 2)
+    elif 0.25 <= guess < 0.5 and 0 < slip <= 0.25:
+        b = round(random.uniform(0.4, 0.5), 2)
+    elif 0.5 <= guess < 0.75 and 0 < slip <= 0.25:
+        b = round(random.uniform(0.25, 0.4), 2)
+    elif 0.75 <= guess < 1 and slip <= 0.25:
+        b = round(random.uniform(0.1, 0.25), 2)
     elif guess == 1:
-        theta = round(random.uniform(0, 0.1), 2)
+        b = round(random.uniform(0, 0.1), 2)
 
-    elif 0.25 <= sleep < 0.5 and 0 < guess <= 0.25:
-        theta = round(random.uniform(0.5, 0.6), 2)
-    elif 0.5 <= sleep < 0.75 and 0 < guess <= 0.25:
-        theta = round(random.uniform(0.6, 0.75), 2)
-    elif 0.75 <= sleep < 1 and guess <= 0.25:
-        theta = round(random.uniform(0.75, 0.9), 2)
-    elif sleep == 1:
-        theta = round(random.uniform(0.9, 1), 2)
+    elif 0.25 <= slip < 0.5 and 0 < guess <= 0.25:
+        b = round(random.uniform(0.5, 0.6), 2)
+    elif 0.5 <= slip < 0.75 and 0 < guess <= 0.25:
+        b = round(random.uniform(0.6, 0.75), 2)
+    elif 0.75 <= slip < 1 and guess <= 0.25:
+        b = round(random.uniform(0.75, 0.9), 2)
+    elif slip == 1:
+        b = round(random.uniform(0.9, 1), 2)
 
     else:
-        theta = round(random.uniform(0.35, 0.65), 2)
+        b = round(random.uniform(0.35, 0.65), 2)
 
 
     #Append lists
@@ -177,8 +291,8 @@ for quiz in range(0, number_Quizzes):
     lst_succes_prct.append(succes_percent)
     lst_failure_prct.append(failure_percent)
     lst_guess.append(guess)
-    lst_sleep.append(sleep)
-    lst_theta.append(theta)
+    lst_slip.append(slip)
+    lst_b.append(b)
 
 
 Quizzes_db = pd.DataFrame({'Quizz_ID': lst_quiz_ID,
@@ -188,19 +302,18 @@ Quizzes_db = pd.DataFrame({'Quizz_ID': lst_quiz_ID,
                            'Success_Percent': lst_succes_prct,  
                            'Failure_Percent': lst_failure_prct,    
                            'Guess': lst_guess,
-                           'Sleep': lst_sleep,
-                           'Theta': lst_theta,      
-}).sort_values(by = 'Quizz_ID', ascending = True)
-
+                           'Slip': lst_slip,
+                           'B': lst_b,      
+}).sort_values(by = 'Quizz_ID', ascending = True).set_index('Quizz_ID')
 
 print(Quizzes_db)
-
+"""
 #############
 # Save Data #
 #############
 
 #Save the Login into a CSV File.
-Quizzes_db.to_csv(path_data + 'Quizzes.csv')
+Quizzes_db.to_csv(path_data + 'QuizzesBis.csv')
 
 #######################
 # Time Execution DONE #
@@ -208,3 +321,5 @@ Quizzes_db.to_csv(path_data + 'Quizzes.csv')
 
 Total_Time = round(time.time() - start_time)
 print("Program Execution Time: %s seconds" % (Total_Time))
+
+
--- Reingenieurie/Factice-Example/Programs/Quizz_VIZ.py
+++ Reingenieurie/Factice-Example/Programs/Quizz_VIZ.py
@@ -7,6 +7,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
+from numpy import median
+from seaborn import palettes
 
 ###################################
 #Define the Path and Folders Name #
@@ -33,6 +35,21 @@ random.seed( 1050 ) #The seed does not need to be randomize.
 # Class and Function #
 ######################
 
+#Function to display the values above the barplots.
+def show_values_on_bars(axs):
+    def _show_on_single_plot(ax):        
+        for p in ax.patches:
+            _x = p.get_x() + p.get_width() / 2
+            _y = p.get_y() + p.get_height() + 1
+            value = '{:.0f}'.format(p.get_height())
+            ax.text(_x, _y, value, ha="center") 
+
+    if isinstance(axs, np.ndarray):
+        for idx, ax in np.ndenumerate(axs):
+            _show_on_single_plot(ax)
+    else:
+        _show_on_single_plot(axs)
+
 
 #################################
 # Import the required Data file #
@@ -45,4 +62,206 @@ Quizz = pd.read_csv(path_Data + 'Quizzes.csv')
 # Visualizations #
 ##################
 
-print(Quizz)
+
+#####################
+# Additional Values #
+#####################
+
+#Create a list of conditions to define the Niveau of digitalization.
+conditions = [
+    (Quizz['B'] > 0.80),
+    (Quizz['B'] > 0.50) & (Quizz['B'] <= 0.80),
+    (Quizz['B'] > 0.20) & (Quizz['B'] <= 0.50),
+    (Quizz['B'] <= 0.20),
+]
+
+#Create a list of values to assign to each condition from the above list
+types = ['Difficile', 'Moyen-Difficile', 'Moyen-Facile', 'Facile']
+
+#Implement the Niveau of digitalization on the dataframe
+Quizz['Niveau'] = np.select(conditions, types)
+Quizz = Quizz.sort_values(by = 'B', ascending = True)
+
+
+print(Quizz.head(5))
+
+###########################################################################################
+# Répartition du Nombre de Tentatives en function du Niveau de Difficultés de chaque Quiz #
+###########################################################################################
+"""
+#Build the figure
+sns.set() # Setting seaborn as default style even if use only matplotlib
+sns.set_style("white")
+
+#Set the size of the figures 
+fig, ax = plt.subplots(1, 1, figsize=(16, 9), sharey=True)
+
+#Axis
+ax = sns.boxplot(x = 'Niveau', y = 'Number_Attempts', data = Quizz)
+ax.set_xlabel("Niveau de Difficultés", size=16)
+ax.set_xticklabels(Quizz['Niveau'].unique(), fontsize=16)
+ax.set_ylabel("Nombre de Tentatives", size=16)
+
+fig.suptitle("Répartition du Nombre de Tentatives en function du Niveau de Difficultés de chaque Quiz", ha = 'center', size=18)
+fig.tight_layout()
+
+#Save Figure
+fig.savefig(path_Figures + "Boxplot of the Distribution of Number of Attempts per Niveau of Difficulty of each Quizz")
+
+#Display Figure
+#plt.show()
+"""
+
+#########################################################################
+# Répartition du Nombre de Réussites par Nombre d'Échecs de chaque Quiz #
+#########################################################################
+"""
+#Build the figure
+sns.set() # Setting seaborn as default style even if use only matplotlib
+sns.set_style("white")
+
+#Set the size of the figures 
+
+#Axis
+sns.relplot(data=Quizz, x='Number_Success', y = 'Number_Failure', hue = 'Niveau', height=6, aspect=1.5)
+plt.title("Nombre de Réussites par le Nombre d'Échecs pour chaque Quiz", size = 18)
+plt.xlabel("Nombre de Réussites", size = 16)
+plt.ylabel("Nombre d'Échec", size = 16)
+plt.tight_layout()
+
+#Save Figure
+#fig.savefig(path_Figures + "Relplot of the Number of Success per Number of Failures for each Quizz")
+
+#Display Figure
+plt.show()
+"""
+
+####
+"""
+#Build the figure
+sns.set() # Setting seaborn as default style even if use only matplotlib
+sns.set_style("white")
+
+#Set the size of the figures 
+fig, ax = plt.subplots(1, 1, figsize=(16, 9), sharey=True)
+
+#Axis
+sns.lineplot(data=Quizz, x='Quizz_ID', y = 'Number_Attempts', palette="flare", ax = ax)
+ax.set_xlabel("Niveau de Difficultés", size=16)
+ax.set_ylabel("Nombre de Tentatives", size=16)
+
+plt.suptitle("Nombre de Réussites par le Nombre d'Échecs pour chaque Quiz", size = 18)
+plt.subplots_adjust(
+top=0.9,
+bottom=0.1,
+left=0.05,
+right=0.9,
+hspace=0.2,
+wspace=0.04
+)
+
+#Save Figure
+#fig.savefig(path_Figures + "Boxplot of the Distribution of Number of Attempts per Level of Difficulty of each Quiz")
+
+#Display Figure
+plt.show()
+"""
+
+####
+"""
+lst_randID =  random.sample(range(0, 10), 5)
+lst_randID = np.arange(1, 11, 1)
+print(lst_randID)
+
+Quizz_randID = Quizz[Quizz.Quizz_ID.isin(lst_randID)]
+print(len(Quizz_randID))
+
+
+plt.bar(Quizz_randID['Quizz_ID'] - 0.25, Quizz_randID['Number_Attempts'], color = 'b', width = 0.25)
+plt.bar(Quizz_randID['Quizz_ID'] , Quizz_randID['Number_Success'], color = 'g', width = 0.25)
+plt.bar(Quizz_randID['Quizz_ID'] + 0.25, Quizz_randID['Number_Failure'], color = 'r', width = 0.25)
+plt.xticks(Quizz_randID['Quizz_ID'])
+plt.show()
+
+# plot bars in stack manner
+plt.bar(Quizz['Quizz_ID'], Quizz['Number_Attempts'], color='r')
+plt.bar(Quizz['Quizz_ID'], Quizz['Number_Success'], color='g')
+plt.bar(Quizz['Quizz_ID'], Quizz['Number_Failure'], color='b')
+plt.show()
+"""
+
+
+
+##############################################################
+# Difficulté et Nombre de Tentative d'un échantillon de Quiz #
+##############################################################
+"""
+lst_randID = np.arange(1, 26, 1)
+Quizz_randID = Quizz[Quizz.Quizz_ID.isin(lst_randID)].sort_values(by = "Quizz_ID", ascending = True)
+print(len(Quizz_randID))
+
+#Build the figure
+sns.set_theme()
+sns.set_style("white")
+
+#Set the size of the figures 
+fig, ax1 = plt.subplots(figsize=(16, 9))
+ax2 = ax1.twinx()
+
+#Define the Plots
+sns.lineplot(data = Quizz_randID['B'], marker='o', sort = False, color = 'black', ax=ax1)
+sns.barplot(data = Quizz_randID, x='Quizz_ID', y='Number_Attempts', ci = 95, hue = 'Niveau', palette = 'Set1', alpha = 0.6, dodge = False, ax=ax2)
+
+#Axis
+ax1.set_ylabel("Difficulté du Quiz (théta)", size=16)
+ax1.set_xlabel("Identifiant du Quiz", size=16)
+ax2.set_ylabel("Nombre de Tentatives", size=16)
+ax2.legend(loc='center', bbox_to_anchor=(1.15, 0.5), shadow=True, title = 'Niveau')
+
+#Last Setting
+fig.suptitle("Nombre de Tentative et Difficulté d'un échantillon de Quiz", ha = 'center', size=18)
+fig.tight_layout()
+
+#Save Figure
+fig.savefig(path_Figures + "Difficulté et Nombre de Tentative d'un échantillon de Quiz")
+
+#Display
+plt.show()
+"""
+
+
+##############################################################
+# Difficulté et Nombre de Tentative d'un échantillon de Quiz #
+##############################################################
+
+lst_randID = np.arange(1, 26, 1)
+Quizz_randID = Quizz[Quizz.Quizz_ID.isin(lst_randID)].sort_values(by = "Quizz_ID", ascending = True)
+print(len(Quizz_randID))
+
+#Build the figure
+sns.set_theme()
+sns.set_style("white")
+
+#Set the size of the figures 
+fig, ax1 = plt.subplots(figsize=(16, 9))
+ax2 = ax1.twinx()
+
+#Define the Plots
+sns.lineplot(data = Quizz_randID['B'], marker='o', sort = False, color = 'black', ax=ax1)
+sns.barplot(data = Quizz_randID, x='Quizz_ID', y='Number_Attempts', ci = 95, hue = 'Niveau', palette = 'Set1', alpha = 0.6, dodge = False, ax=ax2)
+
+#Axis
+ax1.set_ylabel("Difficulté du Quiz (B)", size=16)
+ax1.set_xlabel("Identifiant du Quiz", size=16)
+ax2.set_ylabel("Nombre de Tentatives", size=16)
+ax2.legend(loc='center', bbox_to_anchor=(1.15, 0.5), shadow=True, title = 'Niveau')
+
+#Last Setting
+fig.suptitle("Nombre de Tentative et Difficulté d'un échantillon de Quiz", ha = 'center', size=18)
+fig.tight_layout()
+
+#Save Figure
+fig.savefig(path_Figures + "Difficulté et Nombre de Tentative d'un échantillon de Quiz")
+
+#Display
+plt.show()
